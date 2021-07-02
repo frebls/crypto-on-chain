24a8adf842710f1c67f0d013a9b5902c7b91b1c9
Francesco Balsamo <balsamo90@gmail.com>
2021-07-02 17:41:31 +0200

    update

--- data_retrieval/test_pySpark.py
+++ data_retrieval/test_pySpark.py
@@ -2,30 +2,30 @@
 """BigQuery I/O PySpark example."""
 from pyspark.sql import SparkSession
 
-spark = SparkSession \
-  .builder \
+spark = SparkSession.builder \
   .master('yarn') \
-  .appName('spark-bigquery-demo') \
+  .appName('spark-bigquery-crypto') \
+  .config('spark.jars', 'gs://spark-lib/bigquery/spark-bigquery-latest_2.12.jar') \
   .getOrCreate()
 
 # Use the Cloud Storage bucket for temporary BigQuery export data used
 # by the connector.
-bucket = "[bucket]"
+bucket = "dataproc-temp-us-central1-397709471406-ybhgchsn"
 spark.conf.set('temporaryGcsBucket', bucket)
 
 # Load data from BigQuery.
 words = spark.read.format('bigquery') \
-  .option('table', 'bigquery-public-data:samples.shakespeare') \
+  .option('table', 'bigquery-public-data:crypto_ethereum.transactions') \
   .load()
-words.createOrReplaceTempView('words')
+words.createOrReplaceTempView('eth_transactions')
 
 # Perform word count.
 word_count = spark.sql(
-    'SELECT word, SUM(word_count) AS word_count FROM words GROUP BY word')
+    '''SELECT * FROM eth_transactions WHERE DATE(block_timestamp) between "2020-01-01" and "2020-01-31"''')
 word_count.show()
 word_count.printSchema()
 
 # Saving the data to BigQuery
 word_count.write.format('bigquery') \
-  .option('table', 'wordcount_dataset.wordcount_output') \
+  .option('table', 'crypto_ethereum.transactions') \
   .save()
